5c7fb1a24eb3bf81776bd8b72fc69dae4b5dc5a4
Mustafa yenişehirli <you@example.com191805024@stu.adu.edu.tr>
2023-09-04 20:07:46 +0300

    feat : create message model

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from contact.models import Message
 
 # Register your models here.
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['id','name', 'email', 'subject', 'message', 'created_date', 'updated_date']
+    search_fields = ['name', 'email', 'subject', 'message', ]
+
+    class Meta:
+        model = Message
--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from contact.models import Message
 
 
 # Create your views here.
 
 def contact_form(request):
+    if request.POST:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        Message.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message,
+        )
+
+        success = True
+        message = 'Contact form sent successfly.'
+    else:
+        success = False
+        message = 'Request method is not valid.'
     context = {
-        'success': True,
-        'message': 'Contact form sent successfly.',
+        'success': success,
+        'message': message,
     }
     return JsonResponse(context)
 
