d8c66d720bdd1bba9a5ab391e6035fc96bfacd9f
Mustafa yenişehirli <you@example.com191805024@stu.adu.edu.tr>
2023-09-07 21:30:14 +0300

    Merge branch 'main' of github.com:yenisehirli/Resume-Training

--- resume/settings.py
+++ resume/settings.py
@@ -35,8 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS', cast =list)
 
-#CSRF_TRUSTED_ORIGINS = env('CSRF_TRUSTED_ORIGINS',cast=list)
-CSRF_TRUSTED_ORIGINS=['http://*.127.0.0.1','http://*.localhost','http://*.mustafayenisehirli.com']
+CSRF_TRUSTED_ORIGINS = env('CSRF_TRUSTED_ORIGINS',cast=list)
 
 
 # Application definition
